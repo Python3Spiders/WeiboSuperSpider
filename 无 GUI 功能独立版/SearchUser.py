3793367c8d87c155e91637329d12bfc89f9b353e
月小水长 <2391527690@qq.com>
2022-06-02 16:43:27 +0800

    update xpath of search user

--- 无 GUI 功能独立版/SearchUser.py
+++ 无 GUI 功能独立版/SearchUser.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # author:           inspurer(月小水长)
-# create_time:      2021/10/20 22:21
+# create_time:      2022/6/2 16:36
 # 运行环境           Python3.6+
 # github            https://github.com/inspurer
+# website           https://buyixiao.github.io/
 # 微信公众号         月小水长
 
 import requests
@@ -12,7 +13,7 @@ from lxml import etree
 
 def parseResponse(response):
     html = etree.HTML(response.text)
-    users = html.xpath('//div[starts-with(@class,"card card-user-b")]/div[@class="info"]/div/a[last()-1]/@href')
+    users = html.xpath('//div[starts-with(@class,"card card-user-b")]/div[@class="avator"]/a/@href')
     if len(users) == 0:
         return -1
     temp = users[0]
@@ -22,7 +23,7 @@ def parseResponse(response):
 
 def getUidByName(name):
     # https://s.weibo.com/user?q=%E6%B5%8B%E8%AF%95&Refer=weibo_user
-    cookie = '复制上面的已登录的 cookie'
+    cookie = 'SINAGLOBAL=9725772483514.139.1613553315797; AMCV_98CF678254E93B1B0A4C98A5%40AdobeOrg=1176715910%7CMCMID%7C56647413939893769894172894748680205635%7CMCAAMLH-1652586557%7C3%7CMCAAMB-1652586557%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1651988957s%7CNONE%7CvVersion%7C5.4.0; _ga=GA1.2.648413773.1654057924; __gpi=UID=000006153f241880:T=1654057924:RT=1654057924:S=ALNI_MaR_KL_KDRNTJf3-lGlnepOdne9-A; iUUID=5e06eba520c0e063c718a5796c843783; __gads=ID=6ace8c548753e786-22d2b26ba0d300b0:T=1654057924:RT=1654057939:S=ALNI_MaX1T7rChRO-pDVp6jY21u9tt89Bw; ALF=1685596836; SCF=Ak5UIygEew_0NkA_WvuL8CBEDoRWNdgAQNITQ0vB_Z5_L61DnNwAgMbepgduKihW6ZC1Lo48jv9nZPxVWlzYZQo.; _s_tentry=weibo.com; Apache=5655623160769.916.1654065590766; ULV=1654065590826:256:2:8:5655623160769.916.1654065590766:1654056478315; UOR=,,www.google.com.hk; SSOLoginState=1654067034; SUB=_2A25Pk38KDeRhGeBG7lsQ8y3MyjWIHXVtfAFCrDV8PUJbkNAKLXHAkW1NRhjQcFQJU3-ctzoHQf04W3vlLyl6by9v; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5MCOuIe9TuswxEEPRIFh7X5NHD95Qc1h-4eKe0eh24Ws4Dqc_zi--Xi-i2iK.fi--Xi-iWiKyFi--Xi-zRiKy2i--Ri-isiKnNi--4iK.Ni-2Ri--ciKnRi-zNi--Ni-2NiKnpi--Ri-88i-2p'
 
     headers = {
         'Connection': 'keep-alive',
@@ -94,4 +95,3 @@ def dfAddUserLink(file_path, user_name_column, user_link_column='user_link', fin
 if __name__ == '__main__':
     userLink = getUserLinkByName('水哥')
     print(userLink)
-    dfAddUserLink('test.csv', user_name_column='user_name')
